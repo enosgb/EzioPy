0a8e035ab85386edec3aa25dd31809e028949269
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2021-12-12 19:10:53 -0300

    Add files via upload

--- read_of_keys.py
+++ read_of_keys.py
@@ -1,29 +1,33 @@
 import tkinter as tk
-from tkinter.constants import LEFT, RIGHT, TOP
-from tkinter import Listbox, StringVar, ttk
+from tkinter.constants import ANCHOR, CENTER, LEFT, RIGHT, TOP
+from tkinter import Widget, ttk
+import types
 
 
 window = tk.Tk()
-window.geometry("320x250")
+window.geometry("700x400")
 window.title("Ezio Kill Users")
 
 #def getTextInput():
  #   result=textExample.get(1.0, tk.END+"-1c")
   #  print(result)
 
-lista = Listbox(height=10)
-choices = ["apple", "orange", "banana"]
-choicesvar = StringVar(value=choices)
-lista = Listbox(listvariable=choicesvar)
-#choices.append("peach")
-#choicesvar.set(choices)
-lista.grid(column = 0, row=1)
+
+columns = ('user','session','name')
+
+tree = ttk.Treeview(window,height=15,columns=columns,show='headings')
+tree.heading('user',text='Usuário')
+tree.heading('session',text='Sessão')
+tree.heading('name',text='Nome')
+
+tree.insert('', 'end', text='user', values=('073933','443','Enos Gabriel'))
 
 
+tree.grid(column=0,row=1)
 
 
-txt_usuario = tk.Text(window, height=1.5, width=25)
-txt_usuario.grid(column = 0, row = 0, padx = 10, pady = 10)
+txt_usuario = tk.Text(window, height=1.5, width=75)
+txt_usuario.grid(column = 0, row = 0)
 
 btn_buscar_usuario = tk.Button(window, height = 1, width = 10, text="Buscar")
 btn_buscar_usuario.grid(column = 1, row = 0, padx = 10, pady = 10)
